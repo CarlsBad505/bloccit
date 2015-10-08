c32c1a7d7e8033fa2828a12efb2fb52558c4cf3b
carlsbad505 <carl@tabrific.com>
2015-10-08 16:07:21 +0000

    Finish cp 33 assignment

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,6 +16,9 @@ posts = Post.all
     )
 end
 
+Post.find_or_create_by(title: "Unique title", body: "This is unique body.")
+#Post.comments.find_or_create_by(body: "This is unique comment.", post: post)
+
 puts "Seed Finished"
 puts "#{Post.count} posts created."
 puts "#{Comment.count} comments created."
