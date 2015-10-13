c048d542b40e5f72573b68e7f6c68a34b39510a3
carlsbad505 <carl@tabrific.com>
2015-10-13 23:42:45 +0000

    Finish assignment with error

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,3 +1,7 @@
 class Topic < ActiveRecord::Base
   has_many :posts, dependent: :destroy
+  
+  validates :name, length: { minimum: 5 }, presence: true
+  validates :description, length: { minimim: 15 }, presence: true
+  validates :public, presence: true
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -12,7 +12,7 @@ RSpec.describe Post, type: :model do
   it { should validate_presence_of(:topic) }
   
   it { should validate_length_of(:title).is_at_least(5) }
-  it {should validate_length_of(:body).is_at_least(20) }
+  it { should validate_length_of(:body).is_at_least(20) }
   
   describe "attributes" do
     it "should respond to title" do
