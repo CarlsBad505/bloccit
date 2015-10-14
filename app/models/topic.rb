ab4e0c9cbe267f458728b8aad96ebcc4feee9c54
carlsbad505 <carl@tabrific.com>
2015-10-14 15:48:21 +0000

    Finish cp 38 assignment

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -2,6 +2,5 @@ class Topic < ActiveRecord::Base
   has_many :posts, dependent: :destroy
   
   validates :name, length: { minimum: 5 }, presence: true
-  validates :description, length: { minimim: 15 }, presence: true
-  validates :public, presence: true
+  validates :description, length: { minimum: 15 }, presence: true
 end
