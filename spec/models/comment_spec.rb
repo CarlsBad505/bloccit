4ef5ac64cef474ddab6d805cbc77b6bd1bc4e505
carlsbad505 <carl@tabrific.com>
2015-10-18 01:11:03 +0000

    Begin comments resource

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -5,7 +5,13 @@ RSpec.describe Comment, type: :model do
   let(:topic) { Topic.create!(name: RandomData.random_sentence, description: RandomData.random_paragraph) }
   let(:user) { User.create!(name: "John Snow", email: "John@gmail.com", password: "winteriscoming") }
   let(:post) { topic.posts.create!(title: RandomData.random_sentence, body: RandomData.random_paragraph, user: user) }
-  let(:comment) { Comment.create!(body: "This is a comment", post: post) }
+  let(:comment) { Comment.create!(body: "This is a comment", post: post, user: user) }
+  
+  it { should belong_to(:post) }
+  it { should belong_to(:user) }
+  
+  it { should validate_presence_of(:body) }
+  it { should validate_length_of(:body).is_at_least(5) }
   
   describe "attributes" do
     it "should respond to a comment" do
