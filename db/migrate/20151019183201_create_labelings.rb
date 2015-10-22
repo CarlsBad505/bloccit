e1b33aec7084695d8c2d5ce92d46fd0fb5ceb27d
carlsbad505 <carl@tabrific.com>
2015-10-22 19:41:17 +0000

    Fix migrations

--- db/migrate/20151019183201_create_labelings.rb
+++ db/migrate/20151019183201_create_labelings.rb
@@ -2,16 +2,16 @@ class CreateLabelings < ActiveRecord::Migration
   
   def change
     create_table :labelings do |t|
-      t.integer :label_id
-      t.integer :topic_id
+      #t.integer :label_id
+      #t.integer :topic_id
       t.references :label, index: true
       t.references :topic, index: true
       t.references :post, index: true
       t.references :labelable, polymorphic: true, index: true
       t.timestamps null: false
     end
-    add_foreign_key :labelings, :labels
-    add_foreign_key :labelings, :topics
-    add_foreign_key :labelings, :posts
+    # add_foreign_key :labelings, :labels
+    # add_foreign_key :labelings, :topics
+    # add_foreign_key :labelings, :posts
   end
 end
