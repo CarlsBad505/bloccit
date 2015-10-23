128a56e939b16c3e6175987506223f4d9a58d854
carlsbad505 <carl@tabrific.com>
2015-10-23 19:17:45 +0000

    Attempt assignment

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,26 @@
 module UsersHelper
+  
+  def no_posts?
+    if @user.posts.nil?
+      "#{@user.name} has not submitted any posts yet!"
+    else
+      "Posts"
+    end
+  end
+  
+  def no_comments?
+    if @user.comments.nil?
+      "#{@user.name} has not submitted any comments yet!"
+    else
+      "Comments"
+    end
+  end
+  
+  def no_favorites?
+    if @user.favorites.nil?
+      "#{@user.name} has not favorited any posts yet!"
+    else
+      "Favorites"
+    end
+  end
 end
