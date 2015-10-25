f54b62cda2052fa5be4a5d9ef68bd2438091b732
carlsbad505 <carl@tabrific.com>
2015-10-25 20:41:27 +0000

    Begin assignment

--- config/routes.rb
+++ config/routes.rb
@@ -20,7 +20,12 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :users, only: [:index, :show, :create, :update]
-      resources :topics, except: [:new, :edit]
+      resources :topics, except: [:new, :edit] do
+        resources :posts, only: [:index, :show]
+      end
+      resources :posts, only: [] do
+        resources :comments, only: [:index, :show]
+      end
     end
   end
 end
